1133add349acec459ff71af6bae5b9a01fca3612
Divypatel4919 <162021465+Divypatel4919@users.noreply.github.com>
2024-04-20 09:10:38 +0530

    BOTECS.py

--- BOTECS.py
+++ BOTECS.py
@@ -4,8 +4,8 @@
 # Broadcasting channel Telegram : @Team_Exploit
 # Tool Vip BotECS In channel : @KnightGuardian59
 
-token = ' Insert your bot token  '
-chat_id = 'Insert your account chat id ' 
+token = ' 7095159700:AAG4lf_emIxYJtzucbgIcC4ZCK-YY8zpus0  '
+chat_id = ' 5518039078 ' 
 
 from os import system
 from platform import (system,platform)
